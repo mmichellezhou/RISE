a3b152be4a317f234e6dcdc4d6ea9c355a7a6888
mmichellezhou <65831695+mmichellezhou@users.noreply.github.com>
2023-07-11 16:54:20 -0400

    Implemented algorithm 1 with tests

---
+++ OnlineAdAllocWithPred.py
@@ -0,0 +1,67 @@
+import bisect
+
+class OnlineAdAllocWithPred:
+
+    def __init__(self, b, w):
+        self.budgets = b
+        self.weights = w
+        self.numAdvertisers = len(self.weights)
+        self.numImpressions = len(self.weights[0])
+        self.advertisers = [[] for avd in range(self.numAdvertisers)]
+        self.dummyAdvertiser = []
+        self.thresholds = [0] * self.numAdvertisers
+        
+    def PRD(self, t):
+        return 0
+    
+    def maxDiscGain(self, t, thresholds):
+        max = 0
+        maxAdv = 0
+        for adv in range(0, self.numAdvertisers):
+            discGain = self.weights[adv][t] - thresholds[adv]
+            if (discGain > max):
+                max = discGain
+                maxAdv = adv
+        return maxAdv
+    
+    def updateThresh(self, adv, trust):
+        B = self.budgets[adv]
+        e = (1 + 1 / B) ** B
+        # print(adv, B, e)
+        sum = 0
+        i = 1
+        for t in self.advertisers[adv]:
+            sum += self.weights[adv][t] * e ** ((trust * (B - i)) / B)
+            i += 1
+        return (e ** (trust/B) - 1) * sum / (e ** trust - 1)
+    
+    def algorithm1(self, trust):
+        B = min(self.budgets)
+        e = (1 + 1 / B) ** B
+        a = B * (e ** (trust / B) - 1)
+        # print(B, e, a)
+        for t in range(self.numImpressions):
+            advPRD = self.PRD(t)
+            advEXP = self.maxDiscGain(t, self.thresholds)
+            discGainPRD = self.weights[advPRD][t] - self.thresholds[advPRD]
+            discGainEXP = self.weights[advEXP][t] - self.thresholds[advEXP]
+            # if discounted gain is negative, continue to next impression
+            if (discGainPRD < 0 and discGainEXP < 0):
+                self.dummyAdvertiser.append(t)
+                continue
+            if (a * discGainPRD >= discGainEXP):
+                adv = advPRD
+            else:
+                adv = advEXP
+            # if advertiser a has reached their maximum budget, remove least valuable impression assigned to a
+            if len(self.advertisers[adv]) == self.budgets[adv]:
+                self.dummyAdvertiser.append(self.advertisers[adv].pop(0))
+            bisect.insort(self.advertisers[adv], t)
+            self.thresholds[adv] = self.updateThresh(adv, trust)
+        return self.advertisers
+    
+    def getDummyAdvertiser(self):
+        return self.dummyAdvertiser
+
+    def getThresholds(self):
+        return self.thresholds
---
+++ TestOnlineAdAllocWithPred.py
@@ -0,0 +1,45 @@
+from OnlineAdAllocWithPred import OnlineAdAllocWithPred
+import time
+
+class TestOnlineAdAllocWithPred:
+
+    def __init__(self, b, w):
+        self.OAAWP = OnlineAdAllocWithPred(b, w)
+        self.runtime = 0
+
+    def runAlgorithm1(self, trust):
+        start = time.time()
+        res = self.OAAWP.algorithm1(trust)
+        end = time.time()
+        self.runtime = end - start
+        for i in range(len(res)):
+            print("Advertiser " + str(i) + ": " + str(res[i]))
+
+    def printDummyAdvertiser(self):
+        print("Dummy advertiser: " + str(self.OAAWP.getDummyAdvertiser()))
+
+    def printThresholds(self):
+        print("Thresholds: " + str(self.OAAWP.getThresholds()))
+    
+    def printRuntime(self):
+        print("Runtime: " + str(self.runtime) + " s")
+
+    def runAllTests(self, trust):
+        self.runAlgorithm1(trust)
+        self.printDummyAdvertiser()
+        self.printThresholds()
+        self.printRuntime()
+    
+if __name__ == "__main__":
+    print("---------- test 1 ----------")
+    test1 = TestOnlineAdAllocWithPred([2, 1], [[2, 1, 10], [100, 2, 1]])
+    test1.runAllTests(1)
+    print("---------- test 2 ----------")
+    test2 = TestOnlineAdAllocWithPred([1, 1], [[0, 2, 50], [3, 10, 30]])
+    test2.runAllTests(1)
+    print("---------- test 3 ----------")
+    test3 = TestOnlineAdAllocWithPred([3, 2], [[2, 10, 4, 0], [1, 16, 0, 9]])
+    test3.runAllTests(1)
+
+    
+    
